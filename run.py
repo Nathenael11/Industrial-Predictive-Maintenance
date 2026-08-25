4881c9ef6bb72494f7bc55e7333f280fc17204f4
Nathenael <nathenael11@gmail.com>
2026-08-24 22:13:29 -0700

    Add render.yaml for Python deployment

--- run.py
+++ run.py
@@ -1,9 +1,4 @@
-"""
-Run script for Predictive Maintenance Flask Web App
-"""
-
 from app import app
 
-if __name__ == '__main__':
-    print("Starting Elevvo Predictive Maintenance Web Application on http://localhost:5000...")
-    app.run(host='0.0.0.0', port=5000, debug=True)
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
